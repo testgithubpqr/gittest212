008564e425598b3d6426c4af809cfb13b8d27155
tester <ashwini.rao2105@gmail.com>
2025-05-23 12:54:26 +0530

    first commit m

---
+++ src/test/java/gittest212/test.java
@@ -0,0 +1,11 @@
+package gittest212;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("master first commit");
+		
+	}
+
+}
